f9af01127671008f16b803c788e27b1053563e09
kaleissin <kaleissin@gmail.com>
2013-12-02 11:39:30 +0100

    Be independent of 3rd party discover_runner

--- src/mailer/settings/test.py
+++ src/mailer/settings/test.py
@@ -15,9 +15,5 @@ SECRET_KEY = 'na2Tei0FoChe3ooloh5Yaec0ji7Aipho'
 SOUTH_TESTS_MIGRATE = False
 SKIP_SOUTH_TESTS = True
 
-if django.VERSION[:2] < (1, 6):
-    TEST_RUNNER = 'discover_runner.DiscoverRunner'
-    TEST_DISCOVER_ROOT = SITE_ROOT
-    INSTALLED_APPS += ('discover_runner',)
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
 TEST_DISCOVER_PATTERN = "*"
